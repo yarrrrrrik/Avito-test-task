641531f6b1cd8f9b480db4979b401c632faf7705
yarrrrrrik <60892661+yarrrrrrik@users.noreply.github.com>
2020-06-24 02:15:49 +0500

    View component almost done

--- src/index.js
+++ src/index.js
@@ -12,47 +12,41 @@ class Repo {
       this.languages_url = languages_url;
       this.avatar_url = avatar_url;
       this.login = login;
+      this.languages = '';
+      this.contributors = ''
   }
 }
 
 let model = {
   response:'',
-
   reposArr:[],
-
-  pushReposToArr: (reposArr,response) => {
-
-  },
 
   getRepos: async (searchValue = 'stars:>0',page = '1') => {
     model.response = ''
-
     model.reposArr = []
-    console.log(searchValue);
-    console.log(page);
-    // https://api.github.com/search/repositories?q=stars:%3E0&sort=stars&order=desc&page=1
-    // GET /repos/:owner/:repo/stats/contributors
-    // https://api.github.com/repos/freeCodeCamp/freeCodeCamp/stats/commit_activity
-    // "contributors_url": "https://api.github.com/repos/vuejs/vue/contributors",
-    // "description": "🖖 Vue.js is a progressive, incrementally-adoptable JavaScript framework for building UI on the web.",
 
     let request = await fetch(`https://api.github.com/search/repositories?q=${searchValue}&sort=stars&order=desc&page=${page}&per_page=10`)
     model.response = await request.json()
     model.response.items.forEach((item, i) => {
-        console.log(item.owner.avatar_url);
         let repo = new Repo(item.name,item.stargazers_count,item.updated_at,item.html_url,item.owner.avatar_url,item.owner.login,item.description,item.contributors_url,item.languages_url)
         model.reposArr.push(repo)
-        console.log(model.reposArr)
-
     });
   },
+  getRepoCardInfo: async (repo) => {
+    let languages = await fetch(repo.languages_url)
+    repo.languages = await languages.json()
+
+    let contributors = await fetch(repo.contributors_url)
+    repo.contributors = await contributors.json()
+  }
 }
+
 let view = {
   inputValue:undefined,
-  makeRepoNodeElement:(repo) => {
+
+  makeRepoNodeElement:(repo,i) => {
     let repoNode = document.createElement('div')
     repoNode.classList.add('repo-card')
-
     let repoName = document.createElement('h3')
     repoName.innerHTML = `${repo.name}`
     let repoStars = document.createElement('p')
@@ -60,11 +54,10 @@ let view = {
     let repoUpdated = document.createElement('p')
     repoUpdated.innerHTML = `${repo.updated}`
     let repoLink = document.createElement('a')
-    repoLink.classList.add('card-link')
+    repoLink.addEventListener('click',controller.openCardEvent)
+    repoLink.classList.add(`card-link`)
+    repoLink.id = i
     repoLink.innerHTML = 'Заглянуть'
-
-    // repoLink.href = `${repo.link}`
-
     repoLink.addEventListener('click',(e) => {
       document.querySelector('.block').classList.add('hidden')
     })
@@ -72,14 +65,55 @@ let view = {
     repoNode.appendChild(repoStars)
     repoNode.appendChild(repoUpdated)
     repoNode.appendChild(repoLink)
-
     document.querySelector('.items').appendChild(repoNode)
+  },
+  makeRepoCardNodeElement:(repo) => {
+    let repoNode = document.createElement('div')
+    repoNode.classList.add('repo-big-card')
+    repoNode.insertAdjacentHTML('afterbegin',`
+      <div class="repo-big-card__header">
+        <a href="">Назад</a>
+        <h3>${repo.name}</h3>
+        <p>★${repo.stars}</p>
+        <p>${repo.updated_at}</p>
+      </div>
+      <div class="repo-big-card__user">
+        <img src="${repo.avatar_url}" alt="">
+        <a href="#">${repo.login}</a>
+      </div>
+      <div class="repo-big-card__description">
+        <div class="languages">
+          <p>Python</p>
+          <p>C++</p>
+          <p>Pascal</p>
+        </div>
+        <div class="repo-big-card__info">
+          <p>${repo.description}</p>
+        </div>
+        <div class="contributors"></div>
+      </div>
+    `)
+    document.querySelector('body').appendChild(repoNode)
+
+    // let repoHeader = document.createElement('div')
+    // repoHeader.classList.add('repo-big-card__header')
+    // let repoName = document.createElement('h3')
+    // repoName.innerHTML = `${repo.name}`
+    // let repoStars = document.createElement('p')
+    // repoStars.innerHTML = `★${repo.stars}`
+    // let repoUpdated = document.createElement('p')
+    // repoUpdated.innerHTML = `${repo.updated}
+    // let repoUser = document.createElement('div')
+    // repoUser.classList.add('repo-big-card__user')
+    // let repoAvatar = document.createElement('img')
+    // repoAvatar.src = repo.avatar_url
+    // let repoLogin = document.createElement('a')
+    // repoLogin = document.createElement()
+
   },
   inputEvent:() => {
     document.querySelector('#search-btn').addEventListener('click',controller.inputEvent)
-
   },
-
   paginationEvents:() => {
     let pageButtons = document.querySelectorAll('.pagination_button')
     pageButtons.forEach((item, i) => {
@@ -90,17 +124,17 @@ let view = {
       item.addEventListener('blur',(e) => {
         e.target.classList.remove('active-btn')
       })
-
     });
 
   }
+
 }
 
 let controller = {
   getRepos: async (searchValue,page) => {
     await model.getRepos(searchValue,page)
     await model.reposArr.forEach((item, i) => {
-      view.makeRepoNodeElement(item)
+      view.makeRepoNodeElement(item,i)
     });
   },
   inputEvent:() => {
@@ -108,31 +142,23 @@ let controller = {
 
     controller.getRepos(view.inputValue)
 
-
     let element = document.querySelector('.items')
     while (element.firstChild) {
       element.removeChild(element.firstChild);
     }
   },
   paginationEvents: async (e) => {
-    // console.log(e.target.innerText);
-    // controller.method(e.target.innerText)
     let element = document.querySelector('.items')
     while (element.firstChild) {
       element.removeChild(element.firstChild);
     }
-
-    // await model.getRepos(searchValue)
-    // await model.reposArr.forEach((item, i) => {
-    //   view.makeRepoNodeElement(item)
-    // });
-    // console.log(view.inputValue);
     controller.getRepos(view.inputValue,e.target.innerText)
-    console.log(typeof view.inputValue);
-    // model.reposArr.forEach((item, i) => {
-    //     view.makeRepoNodeElement(item)
-    // })
     },
+  openCardEvent:async (e) => {
+    await model.getRepoCardInfo(model.reposArr[e.target.id])
+    await view.makeRepoCardNodeElement(model.reposArr[e.target.id])
+    // await console.log(model.reposArr);
+  }
   }
 
 controller.getRepos()
